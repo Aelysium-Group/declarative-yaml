7a1e4bbe9355d052c82151702cea11d1a6397713
immohika <mohi222@pm.me>
2024-10-05 15:28:58 +0330

    support records

--- src/main/java/group/aelysium/declarative_yaml/FieldAssigner.java
+++ src/main/java/group/aelysium/declarative_yaml/FieldAssigner.java
@@ -30,27 +30,33 @@ class FieldAssigner {
     ));
 
     public static Object serialize(CommentedConfigurationNode node, Class<?> clazz, Type type) throws Exception {
-        if(Primitives.isPrimitive(clazz)) return FieldAssigner.serializePrimitive(node, clazz);
-        if(String.class.isAssignableFrom(clazz)) return FieldAssigner.serializeString(node);
-        if(Serializable.class.isAssignableFrom(clazz)) return FieldAssigner.serializeObject(node, clazz);
+        if (Primitives.isPrimitive(clazz)) return FieldAssigner.serializePrimitive(node, clazz);
+        if (String.class.isAssignableFrom(clazz)) return FieldAssigner.serializeString(node);
+        if (Serializable.class.isAssignableFrom(clazz)) return FieldAssigner.serializeObject(node, clazz);
         if (clazz.isEnum()) {
             @SuppressWarnings("unchecked") final var enumType = (Class<? extends Enum<?>>) clazz;
             return FieldAssigner.serializeEnum(node, enumType);
         }
+        if (Record.class.isAssignableFrom(clazz)) return FieldAssigner.serializeRecord(node, clazz);
 
-        if(!(type instanceof ParameterizedType parameterizedType))
-            throw new RuntimeException(clazz.getSimpleName()+" is not a type that's supported by Declarative YAML. Supported types are: "+supportedTypes);
+        if (!(type instanceof ParameterizedType parameterizedType))
+            throw new RuntimeException(clazz.getSimpleName() + " is not a type that's supported by Declarative YAML. Supported types are: " + supportedTypes);
 
-        if(List.class.isAssignableFrom(clazz) && node.isList()) return FieldAssigner.serializeList(node, clazz, parameterizedType);
-        if(Set.class.isAssignableFrom(clazz) && node.isList()) return FieldAssigner.serializeSet(node, clazz, parameterizedType);
-        if(Map.class.isAssignableFrom(clazz) && node.isMap()) return FieldAssigner.serializeMap(node, clazz, parameterizedType);
+        if (List.class.isAssignableFrom(clazz) && node.isList())
+            return FieldAssigner.serializeList(node, clazz, parameterizedType);
+        if (Set.class.isAssignableFrom(clazz) && node.isList())
+            return FieldAssigner.serializeSet(node, clazz, parameterizedType);
+        if (Map.class.isAssignableFrom(clazz) && node.isMap())
+            return FieldAssigner.serializeMap(node, clazz, parameterizedType);
 
-        throw new RuntimeException(clazz.getSimpleName()+" is not a type that's supported by Declarative YAML. Supported types are: "+supportedTypes);
+        throw new RuntimeException(clazz.getSimpleName() + " is not a type that's supported by Declarative YAML. Supported types are: " + supportedTypes);
     }
+
 
     private static <T> T serializePrimitive(CommentedConfigurationNode node, Class<T> clazz) throws Exception {
         return node.get(clazz);
     }
+
     private static String serializeString(CommentedConfigurationNode node) {
         return node.getString();
     }
@@ -72,7 +78,7 @@ class FieldAssigner {
                     CommentedConfigurationNode node = DeclarativeYAML.getNodeFromYAML(objectRoot, key);
                     f.set(instance, serialize(node, f.getType(), f.getGenericType()));
                 } catch (Exception e) {
-                    if(isOptional) {
+                    if (isOptional) {
                         f.set(instance, null);
                         f.setAccessible(false);
                         continue;
@@ -90,7 +96,7 @@ class FieldAssigner {
         }
     }
 
-    private static Object serializeEnum(CommentedConfigurationNode node, Class<?extends Enum<?>> clazz) {
+    private static Object serializeEnum(CommentedConfigurationNode node, Class<? extends Enum<?>> clazz) {
         String value = node.getString();
 
         if (value == null || value.isBlank()) {
@@ -105,45 +111,72 @@ class FieldAssigner {
         }
     }
 
+    private static Object serializeRecord(CommentedConfigurationNode node, Class<?> clazz) throws Exception {
+        RecordComponent[] components = clazz.getRecordComponents();
+        Map<String, Object> values = new HashMap<>();
+
+        for (RecordComponent component : components) {
+            String key = FieldAssigner.convertFieldNameToYAMLKey(component.getName());
+            CommentedConfigurationNode componentNode = node.node(key);
+            values.put(component.getName(), FieldAssigner.serialize(componentNode, component.getType(), component.getGenericType()));
+        }
+
+        Object[] valuesArray = Arrays.stream(components)
+                .map(component -> values.get(component.getName()))
+                .toArray();
+
+        Constructor<?> constructor = clazz.getDeclaredConstructor(
+                Arrays.stream(components).map(RecordComponent::getType).toArray(Class<?>[]::new)
+        );
+        constructor.setAccessible(true);
+        Object instance = constructor.newInstance(valuesArray);
+        constructor.setAccessible(false);
+
+        return instance;
+    }
+
     private static List<Object> serializeList(CommentedConfigurationNode node, Class<?> clazz, ParameterizedType type) throws Exception {
         Type entryType = type.getActualTypeArguments()[0];
         Class<?> entryClass = (Class<?>) type.getActualTypeArguments()[0];
 
-        if(!(Primitives.isPrimitive(entryClass) || String.class.isAssignableFrom(entryClass) || Serializable.class.isAssignableFrom(entryClass)))
-            throw new ClassCastException(clazz.getSimpleName()+" isn't a supported type in Declarative YAML. The supported types are: "+supportedTypes);
+        if (!(Primitives.isPrimitive(entryClass) || String.class.isAssignableFrom(entryClass) || Serializable.class.isAssignableFrom(entryClass)))
+            throw new ClassCastException(clazz.getSimpleName() + " isn't a supported type in Declarative YAML. The supported types are: " + supportedTypes);
 
         List<Object> list = new ArrayList<>();
         for (CommentedConfigurationNode entry : node.childrenList())
             list.add(serialize(entry, entryClass, entryType));
         return list;
     }
+
     private static Set<Object> serializeSet(CommentedConfigurationNode node, Class<?> clazz, ParameterizedType type) throws Exception {
         Type entryType = type.getActualTypeArguments()[0];
         Class<?> entryClass = (Class<?>) type.getActualTypeArguments()[0];
 
-        if(!(Primitives.isPrimitive(entryClass) || String.class.isAssignableFrom(entryClass) || Serializable.class.isAssignableFrom(entryClass)))
-            throw new ClassCastException(clazz.getSimpleName()+" isn't a supported type in Declarative YAML. The supported types are: "+supportedTypes);
+        if (!(Primitives.isPrimitive(entryClass) || String.class.isAssignableFrom(entryClass) || Serializable.class.isAssignableFrom(entryClass)))
+            throw new ClassCastException(clazz.getSimpleName() + " isn't a supported type in Declarative YAML. The supported types are: " + supportedTypes);
 
         Set<Object> set = new HashSet<>();
         for (CommentedConfigurationNode entry : node.childrenList())
             set.add(serialize(entry, entryClass, entryType));
         return set;
     }
+
     private static Map<String, ?> serializeMap(CommentedConfigurationNode node, Class<?> clazz, ParameterizedType type) throws Exception {
         Type keyType = type.getActualTypeArguments()[0];
         Type valueType = type.getActualTypeArguments()[1];
         Class<?> keyClass = (Class<?>) type.getActualTypeArguments()[0];
         Class<?> valueClass = (Class<?>) type.getActualTypeArguments()[1];
 
-        if(!(String.class.isAssignableFrom(keyClass)))
-            throw new ClassCastException(clazz.getSimpleName()+" isn't a supported type in Declarative YAML. The supported types are: "+supportedTypes);
-        if(!(Primitives.isPrimitive(valueClass) || String.class.isAssignableFrom(valueClass) || Serializable.class.isAssignableFrom(valueClass)))
-            throw new ClassCastException(clazz.getSimpleName()+" isn't a supported type in Declarative YAML. The supported types are: "+supportedTypes);
+        if (!(String.class.isAssignableFrom(keyClass)))
+            throw new ClassCastException(clazz.getSimpleName() + " isn't a supported type in Declarative YAML. The supported types are: " + supportedTypes);
+        if (!(Primitives.isPrimitive(valueClass) || String.class.isAssignableFrom(valueClass) || Serializable.class.isAssignableFrom(valueClass)))
+            throw new ClassCastException(clazz.getSimpleName() + " isn't a supported type in Declarative YAML. The supported types are: " + supportedTypes);
 
         Map<String, Object> map = new HashMap<>();
 
         for (Map.Entry<Object, CommentedConfigurationNode> entry : node.childrenMap().entrySet()) {
-            if(!(entry.getKey() instanceof String key)) throw new IllegalArgumentException("Declarative YAML requires that maps loaded from YAML must be of the type ["+supportedMaps+"].");
+            if (!(entry.getKey() instanceof String key))
+                throw new IllegalArgumentException("Declarative YAML requires that maps loaded from YAML must be of the type [" + supportedMaps + "].");
 
             map.put(key, serialize(entry.getValue(), valueClass, valueType));
         }
@@ -154,6 +187,7 @@ class FieldAssigner {
     public static String convertFieldNameToYAMLKey(String name) {
         return String.join(".", Arrays.stream(name.split("_")).map(s -> s.replaceAll("([a-z])([A-Z]+)", "$1-$2").toLowerCase()).toList());
     }
+
     public static String convertFieldNameToYAMLKey(Field field) {
         return convertFieldNameToYAMLKey(field.getName());
     }
